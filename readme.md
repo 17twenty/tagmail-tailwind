5fd5c847fd2f752fc0c0b8bf6191292478873ea0
Nick Glynn <n.s.glynn@gmail.com>
2021-01-31 15:39:08 +1100

    Initial Setup

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Tagmail Tailwind Marketing Site
+
+```bash
+npx tailwindcss-cli@latest build -o full-tailwind.css
+npx tailwindcss-cli@latest build ./src/custom.css -o ./dist/tailwind.css
+```
+
+## Production
+
+```
+NODE_ENV=production npx tailwindcss-cli@latest build ./src/custom.css -o ./dist/tailwind.css
+```
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+
+
+
+  module.exports = {
+    important: true,
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+      ],
+      plugins: [],
+    theme: {
+      fontFamily: {
+        display: ['Gilroy', 'sans-serif'],
+        body: ['Graphik', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          cyan: '#9cdbff',
+        },
+        margin: {
+          '96': '24rem',
+          '128': '32rem',
+        },
+      }
+    },
+    variants: {
+      opacity: ['responsive', 'hover']
+    }
+  }
